843f70029c6ab0b39483f11d0df4224209e219ba
ibrahim elsawy <53919684+ibrahim-elsawy@users.noreply.github.com>
2022-10-01 23:29:28 +0200

    Add files via upload

---
+++ setup-spring-security-demo-database-plaintext.sql
@@ -0,0 +1,53 @@
+DROP DATABASE  IF EXISTS `tweezer`;
+
+CREATE DATABASE  IF NOT EXISTS `tweezer`;
+USE `tweezer`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+('sawy','ibrahimelsawy834@gmail.com','{noop}sawy4507',1),
+('zeinab','zeinabmohamed817@gmail.com','{noop}test123',1);
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('sawy','ROLE_ADMIN'),
+('sawy','ROLE_USER'),
+('zeinab','ROLE_USER');
+
+
+
